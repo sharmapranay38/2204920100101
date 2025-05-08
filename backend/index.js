0760f7d0488e316577687037a7e0d6a823298631
Pranay <sharmapranay38@gmail.com>
2025-05-08 12:04:33 +0530

    added users

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
 import express from 'express'
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
+const AUTH_TOKEN = process.env.AUTH_TOKEN
+const headers = {
+  Authorization: `Bearer ${AUTH_TOKEN}`
+};
+
+app.get('/', (req, res) => {
+  res.send('Server is working!');
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const usersRes = await axios.get('http://20.244.56.144/evaluation-service/users', { headers });
+    res.json(usersRes.data);
+  } catch (error) {
+    console.error('Error fetching users:', error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get('/posts', async (req, res) => {
+  
+});
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
-
-
+  console.log('Server is running on port 3000');
+});
